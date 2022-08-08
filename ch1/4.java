2bc4c4b04b2f871afe57fda2a82c935fe913c14c
zagabi <zagabi@zagabiui-MacBookPro.local>
2022-08-08 12:33:00 +0900

    이름변경

--- ch1/4.java
+++ ch1/4.java
@@ -2,7 +2,7 @@ class Singleton {
     private static class singleInstanceHolder {
         private static final Singleton INSTANCE = new Singleton();
     }
-    public static synchronized Singleton getInstance() {
+    public static Singleton getInstance() {
         return singleInstanceHolder.INSTANCE;
     }
 }
@@ -22,4 +22,12 @@ public class HelloWorld{
 705927765
 705927765
 true
+
+1. 클래스안에 클래스(Holder), static이며 중첩된 클래스인 singleInstanceHolder를 
+기반으로 객체를 선언했기 때문에 한 번만 로드되므로 싱글톤 클래스의 인스턴스는 애플리케이션 당 하나만 존재하며 
+클래스가 두 번 로드되지 않기 때문에 두 스레드가 동일한 JVM에서 2개의 인스턴스를 생성할 수 없습니다. 
+그렇기 때문에 동기화, 즉 synchronized를 신경쓰지 않아도 됩니다. 
+2. final 키워드를 통해서 read only 즉, 다시 값이 할당되지 않도록 했습니다.
+3. 중첩클래스 Holder로 만들었기 때문에 싱글톤 클래스가 로드될 때 클래스가 메모리에 로드되지 않고 
+어떠한 모듈에서 getInstance()메서드가 호출할 때 싱글톤 객체를 최초로 생성 및 리턴하게 됩니다. 
 */
