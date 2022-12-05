8c0b99c6e226112871c9c682d65eb5eec16a282c
kundol <jhc9639@naver.com>
2022-12-05 20:02:49 +0900

    a

--- ch1/13.js
+++ ch1/13.js
@@ -1,3 +1,3 @@
-const ret = [1, 2, 3, 4, 5, 11, 12]
-.reduce((max, num) => num > max ? num : max, 0)
+const list = [1, 2, 3, 4, 5, 11, 12]
+const ret = list.reduce((max, num) => num > max ? num : max, 0)
 console.log(ret) // 12 
