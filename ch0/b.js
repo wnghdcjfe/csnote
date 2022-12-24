0e370236258788f7a8ec6a9e89b0cb81765ded5a
zagabi <jhc9639@naver.com>
2022-12-24 21:12:33 +0900

    Update b.js

--- ch0/b.js
+++ ch0/b.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require('fs')
-app.get('/aaaaa', (req, res) => { 
+app.get('/', (req, res) => {
     const f = JSON.parse(fs.readFileSync("a.json", {encoding:"utf-8"}))
     const data = {
         "name" : f.name
@@ -13,3 +13,4 @@ app.get('/aaaaa', (req, res) => {
 app.listen(port, () => {
   console.log(`http://127.0.0.1:${port}`)
 })
+
